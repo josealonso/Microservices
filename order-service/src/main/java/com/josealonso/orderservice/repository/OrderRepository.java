c4caee3c258af7bd27aebaa0b0820198474e73a8
JR <jose.alonso.programmer@gmail.com>
2022-12-04 19:21:50 +0100

    Implemented the service OrderService.

---
+++ order-service/src/main/java/com/josealonso/orderservice/repository/OrderRepository.java
@@ -0,0 +1,7 @@
+package com.josealonso.orderservice.repository;
+
+import com.josealonso.orderservice.model.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+}
---
+++ order-service/src/main/java/com/josealonso/orderservice/service/OrderService.java
@@ -0,0 +1,65 @@
+package com.josealonso.orderservice.service;
+
+import com.josealonso.orderservice.dto.OrderLineItemsDto;
+import com.josealonso.orderservice.dto.OrderRequest;
+import com.josealonso.orderservice.model.Order;
+import com.josealonso.orderservice.model.OrderLineItems;
+import com.josealonso.orderservice.repository.OrderRepository;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class OrderService {
+
+    private final OrderRepository orderRepository;
+
+    public void placeOrder(OrderRequest orderRequest) {
+        Order order = new Order();
+        order.setOrderNumber(UUID.randomUUID().toString());
+
+        var orderLineItemsList = orderRequest.getOrderLineItemsDtoList()
+                .stream()
+                .map(this::mapToEntity)
+                .toList();
+        order.setOrderLineItemsList(orderLineItemsList);
+        orderRepository.save(order);
+    }
+
+    private OrderLineItems mapToEntity(OrderLineItemsDto orderLineItemsDto) {
+        OrderLineItems orderLineItems = new OrderLineItems();
+        orderLineItems.setPrice(orderLineItemsDto.getPrice());
+        orderLineItems.setQuantity(orderLineItemsDto.getQuantity());
+        orderLineItems.setSkuCode(orderLineItemsDto.getSkuCode());
+        return orderLineItems;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
