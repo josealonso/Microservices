790d0cdd0d69a0aeaf4c4d9497b090506d789689
JR <jose.alonso.programmer@gmail.com>
2022-12-04 19:21:12 +0100

    Implemented the service ProductService.

---
+++ product-service/src/main/java/com/josealonso/productservice/controller/ProductController.java
@@ -0,0 +1,35 @@
+package com.josealonso.productservice.controller;
+
+import com.josealonso.productservice.dto.ProductResponse;
+import com.josealonso.productservice.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/product")
+@RequiredArgsConstructor
+public class ProductController {
+
+    private final ProductService productService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void createProduct(@RequestBody ProductResponse productRequest) {
+        productService.createProduct(productRequest);
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<ProductResponse> getAllProducts() {
+        return productService.getAllProducts();
+    }
+}
+
+
+
+
+
