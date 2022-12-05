33749c977f67e52b96b5ae3162e5a30057e762d1
JR <jose.alonso.programmer@gmail.com>
2022-12-06 00:05:17 +0100

    Used the spring cloud dependency for Spring Boot 3.0.0. Some annotations for the (Eureka) discovery server have changed.

--- inventory-service/src/main/java/com/josealonso/inventoryservice/InventoryServiceApplication.java
+++ inventory-service/src/main/java/com/josealonso/inventoryservice/InventoryServiceApplication.java
@@ -5,11 +5,11 @@ import com.josealonso.inventoryservice.repository.InventoryRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 
-@EnableEurekaClient
 @SpringBootApplication
+@EnableDiscoveryClient
 public class InventoryServiceApplication {
 
     public static void main(String[] args) {
