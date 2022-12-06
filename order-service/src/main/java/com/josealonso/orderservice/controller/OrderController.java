19b7c94045f719d64c64c6c286728db2c99ef12f
JR <jose.alonso.programmer@gmail.com>
2022-12-06 01:46:11 +0100

    Used the Resilience4j API in the Java code.

--- order-service/src/main/java/com/josealonso/orderservice/controller/OrderController.java
+++ order-service/src/main/java/com/josealonso/orderservice/controller/OrderController.java
@@ -2,6 +2,7 @@ package com.josealonso.orderservice.controller;
 
 import com.josealonso.orderservice.dto.OrderRequest;
 import com.josealonso.orderservice.service.OrderService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -15,8 +16,14 @@ public class OrderController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
+    @CircuitBreaker(name = "inventory", fallbackMethod = "fallbackMethod")
     public String placeOrder(@RequestBody OrderRequest orderRequest) {
         orderService.placeOrder(orderRequest);
         return "Order placed successfully";
+    }
+
+    // must return the same type and receive the same params as the "placeOrder" method
+    public String fallbackMethod(OrderRequest orderRequest, RuntimeException runtimeException) {
+        return "Something went wrong, please order after some time!";
     }
 }
